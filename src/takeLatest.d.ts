0a294a84aaf7a0e5ffd2a23a79e97c3d49dd08dc
Sebastian <sebastian.n.rogers@gmail.com>
2025-03-20 00:08:43 -0400

    Work

--- src/takeLatest.d.ts
+++ src/takeLatest.d.ts
@@ -17,19 +17,19 @@ declare interface makeTakeLatest {
     (
       pattern: '*',
       fn: (action: Event) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     /** Constraining signature for predicates */
     <Ev extends Event>(
       fn: (action: Event) => action is Ev,
       fn: (action: Ev) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     /** Expansive signature for predicates */
     (
       fn: (action: Event) => boolean,
       fn: (action: Event) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     // TODO: List of Type Predicates? But its a useless overload? Is it?
 
@@ -37,49 +37,49 @@ declare interface makeTakeLatest {
     <EventType extends Event['type']>(
       pattern: EventType,
       fn: (action: EventDict<Event, EventType>) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     /** Match array of strings */
     <EventType extends Event['type']>(
       pattern: [...EventType[]],
       fn: (action: Event) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     /** Constraining signature for strings AND predicates */
     <EventType extends Event['type']>(
       pattern: [...(EventType | ((action: Event) => action is Simplify<Event & { type: EventType }>))[]],
       fn: (action: Event) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     /** Fallback expansive signature for strings AND predicates */
     (
       pattern: [...(EventType | ((action: Event) => boolean))[]],
-      fn: (action: Event) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+      fn: (action: Event) => SagaGenerator<any, RSES.TAkeLatestEffect>
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     /** Constraining `toString()` signature */
     <Obj extends { toString: () => Event['type'] }>(
       pattern: Obj,
       fn: (action: EventDict<Event, ReturnType<Obj['toString']>>) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     /** Expansive toString() signature */
     (
       pattern: { toString: () => string },
       fn: (action: Event) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     /** Constraining signature for Channels */
     <Channel extends RS.Channel>(
       channel: Channel,
       fn: (action: Event) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
 
     /** Fallback expansive signature for Channels */
     (
       channel: RS.Channel,
       fn: (action: Event) => SagaGenerator
-    ): SagaGenerator<undefined, RSE.TakeLatestEffect>;
+    ): SagaGenerator<undefined, RSE.ForkEffect<never>>;
   }
 }
 
